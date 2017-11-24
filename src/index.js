f5fec3dc88600a37150a42a306a56ca0dfa6c912
Georges Boris <georgesboris@gmail.com>
2017-11-24 11:26:37 -0300

    fix: options expects defaultProps not userDefaultProps

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import getFirebaseSyncHelpers from './utils/helpers';
-import getFirebaseSyncSelector from './utils/selector';
-import getFirebaseSyncMapState from './utils/mapState';
-import getFirebaseSyncConnect from './utils/connect';
-import * as firebaseSyncActions from './redux/actions';
-import getFirebaseSync from './containers/FirebaseSync/FirebaseSync';
+import getFirebaseSyncHelpers from './utils/helpers'
+import getFirebaseSyncSelector from './utils/selector'
+import getFirebaseSyncMapState from './utils/mapState'
+import getFirebaseSyncConnect from './utils/connect'
+import * as firebaseSyncActions from './redux/actions'
+import getFirebaseSync from './containers/FirebaseSync/FirebaseSync'
 
-import getFirebaseSyncReducer from './redux/reducer';
-export { getFirebaseSyncReducer };
+import getFirebaseSyncReducer from './redux/reducer'
+export { getFirebaseSyncReducer }
 
 export default function buildFirebaseSync(options) {
+  const minDefaultProps = {
+    basePath: options.basePath || options.reducerName || 'firebase'
+  }
+  const defaultProps = options.defaultProps
+    ? { ...minDefaultProps, ...options.defaultProps }
+    : minDefaultProps
+  const basePath = defaultProps.basePath
 
-  const minDefaultProps = { basePath: options.basePath || options.reducerName || 'firebase' };
-  const defaultProps = options.userDefaultProps ? { ...minDefaultProps, ...options.userDefaultProps } : minDefaultProps;
-  const basePath = defaultProps.basePath;
-
-  const _getFirebaseSyncHelpers = getFirebaseSyncHelpers(options.firebase, options.store);
-  const _getFirebaseSync = getFirebaseSync(_getFirebaseSyncHelpers);
+  const _getFirebaseSyncHelpers = getFirebaseSyncHelpers(
+    options.firebase,
+    options.store
+  )
+  const _getFirebaseSync = getFirebaseSync(_getFirebaseSyncHelpers)
 
   return {
     getFirebaseSync: _getFirebaseSync,
@@ -26,5 +32,5 @@ export default function buildFirebaseSync(options) {
     firebaseSyncMapState: getFirebaseSyncMapState(basePath),
     firebaseSyncConnect: getFirebaseSyncConnect(basePath),
     firebaseSyncActions
-  };
+  }
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,14 @@
-import getFirebaseRef from '../firebase/getFirebaseRef';
-import { fbSyncItem, fbUnsyncItem, fbSyncList, fbUnsyncList } from '../firebase/read';
-import { setItem, removeItem } from '../redux/actions';
-import getFirebaseSyncSelector from './selector';
-import get from 'lodash/get';
-import isPlainObject from 'lodash/isPlainObject';
+import getFirebaseRef from '../firebase/getFirebaseRef'
+import {
+  fbSyncItem,
+  fbUnsyncItem,
+  fbSyncList,
+  fbUnsyncList
+} from '../firebase/read'
+import { setItem, removeItem } from '../redux/actions'
+import getFirebaseSyncSelector from './selector'
+import get from 'lodash/get'
+import isPlainObject from 'lodash/isPlainObject'
 
 /**
  * ============================================================================
@@ -12,43 +17,50 @@ import isPlainObject from 'lodash/isPlainObject';
  */
 
 function parseItem(snap, props) {
-  let item = snap.exportVal();
+  let item = snap.exportVal()
 
   if (isPlainObject(item)) {
-    item._key = snap.key;
+    item._key = snap.key
   }
 
-  item = props.onProcessItem ? props.onProcessItem(item) : item;
-  item = props.onPostProcessItem ? props.onPostProcessItem(item) : item;
+  item = props.onProcessItem ? props.onProcessItem(item) : item
+  item = props.onPostProcessItem ? props.onPostProcessItem(item) : item
 
-  return item;
+  return item
 }
 
 function onSnap(snap, props, store, innerState, appendKeyToPath, forceRemove) {
-
   /**
    * Parse item.
    */
-    
-  const item = !forceRemove ? parseItem(snap, props) : null;
+
+  const item = !forceRemove ? parseItem(snap, props) : null
 
   /**
    * Parse update path.
    */
 
-  let path = props.localPath ? props.localPath.split('/') : props.path.split('/');
-  if (appendKeyToPath) path = path.concat(snap.key);
+  let path = props.localPath
+    ? props.localPath.split('/')
+    : props.path.split('/')
+  if (appendKeyToPath) path = path.concat(snap.key)
 
   /**
    * Update state.
    */
-  
+
   if (item !== null) {
-    props.onValue && props.onValue(item);
-    props.dispatch(setItem(path, item));
+    props.dispatch(setItem(path, item))
+    if (props.onValue) {
+      if (isPlainObject(item) && item.toJS) {
+        props.onValue(item.toJS())
+      } else {
+        props.onValue(item)
+      }
+    }
   } else {
-    props.onRemove && props.onRemove(snap.key);
-    props.dispatch(removeItem(path));
+    props.onRemove && props.onRemove(snap.key)
+    props.dispatch(removeItem(path))
   }
 
   /**
@@ -56,10 +68,9 @@ function onSnap(snap, props, store, innerState, appendKeyToPath, forceRemove) {
    */
 
   if (innerState && !innerState.loaded) {
-    props.onLoad && props.onLoad();
-    innerState.loaded = true;
+    props.onLoad && props.onLoad()
+    innerState.loaded = true
   }
-
 }
 
 /**
@@ -69,7 +80,6 @@ function onSnap(snap, props, store, innerState, appendKeyToPath, forceRemove) {
  */
 
 function syncItem(props, store, selector) {
-
   /**
    * fbSyncItem returns true if the listener is already active.
    */
@@ -77,50 +87,46 @@ function syncItem(props, store, selector) {
   let innerState = {
     loaded: fbSyncItem({
       ...props,
-      onSnap: (snap) => onSnap(snap, props, store, innerState)
+      onSnap: snap => onSnap(snap, props, store, innerState)
     })
-  };
+  }
 
   /**
    * If already active, trigger onLoad event.
    */
 
   if (innerState.loaded) {
-    props.onLoad && props.onLoad();
+    props.onLoad && props.onLoad()
   }
-
 }
 
 function unsyncItem(props) {
-  return () => fbUnsyncItem(props);
+  return () => fbUnsyncItem(props)
 }
 
 function fetchItem(props, store, selector) {
-
   /**
    * Soft fetch only reaches server if no local item is found.
    */
 
   if (props.fetch === 'soft') {
-
     /**
      * We will try to fetch local item on both mutable and immutable states.
      */
-    
-    const localItem = selector(props)(store.getState());
+
+    const localItem = selector(props)(store.getState())
 
     if (localItem) {
-      props.onLoad && props.onLoad();
-      return Promise.resolve(localItem);
+      props.onLoad && props.onLoad()
+      return Promise.resolve(localItem)
     }
-
   }
 
   /**
    * If fetch is not set as soft,
    * or if no local item was found,
    * reach server and fetch item.
-   * 
+   *
    * For increased cached perfomance,
    * we will actually listen to the object path for a few seconds,
    * by doing it this way we will ensure multiple fetches on the same
@@ -130,24 +136,22 @@ function fetchItem(props, store, selector) {
    */
 
   return new Promise((resolve, reject) => {
-
-    let firstRead = true;
+    let firstRead = true
 
     syncItem({
       ...props,
       onLoad: () => {
         if (firstRead) {
-          firstRead = false;
-          props.onLoad && props.onLoad();
-          resolve(selector(props)(store.getState()));
-          setTimeout(() => fbUnsyncItem(props), 10 * 1000);
+          firstRead = false
+          props.onLoad && props.onLoad()
+          resolve(selector(props)(store.getState()))
+          setTimeout(() => fbUnsyncItem(props), 10 * 1000)
         }
       }
-    });
-
-  });
+    })
+  })
 }
- 
+
 /**
  * ============================================================================
  * Lists
@@ -155,7 +159,6 @@ function fetchItem(props, store, selector) {
  */
 
 function syncList(props, store, selector) {
-
   /**
    * fbSyncList returns true if the listener is already active.
    */
@@ -163,38 +166,37 @@ function syncList(props, store, selector) {
   let innerState = {
     loaded: fbSyncList({
       ...props,
-      onSnapAdded: (snap) => onSnap(snap, props, store, innerState, true),
-      onSnapChanged: (snap) => onSnap(snap, props, store, innerState, true),
-      onSnapRemoved: (snap) => onSnap(snap, props, store, innerState, true, true)
+      onSnapAdded: snap => onSnap(snap, props, store, innerState, true),
+      onSnapChanged: snap => onSnap(snap, props, store, innerState, true),
+      onSnapRemoved: snap => onSnap(snap, props, store, innerState, true, true)
     })
-  };
+  }
 
   /**
    * If already active, trigger onLoad event.
    */
 
   if (innerState.loaded) {
-    props.onLoad && props.onLoad();
-    return;
+    props.onLoad && props.onLoad()
+    return
   }
 
   /**
    * If not trigger the onLoad callback manually after a big timeout.
    * This is necessary because list events does not automatically trigger on empty lists,
-   * 
+   *
    */
 
   setTimeout(() => {
     if (!innerState.loaded) {
-      props.onLoad && props.onLoad();
-      innerState.loaded = true;
+      props.onLoad && props.onLoad()
+      innerState.loaded = true
     }
-  }, 2000);
-  
+  }, 2000)
 }
 
 function unsyncList(props) {
-  return () => fbUnsyncList(props);
+  return () => fbUnsyncList(props)
 }
 
 /**
@@ -204,19 +206,23 @@ function unsyncList(props) {
  */
 
 const getFirebaseSyncHelpers = (firebase, store) => (_defaultProps = {}) => {
-  
-  const _ref = getFirebaseRef(firebase);
-  const defaultProps = { basePath: 'firebase', ..._defaultProps, _ref, dispatch: store.dispatch };
-  const selector = getFirebaseSyncSelector(defaultProps.basePath);
+  const _ref = getFirebaseRef(firebase)
+  const defaultProps = {
+    basePath: 'firebase',
+    ..._defaultProps,
+    _ref,
+    dispatch: store.dispatch
+  }
+  const selector = getFirebaseSyncSelector(defaultProps.basePath)
 
   return {
-    fetchItem: (props)  => fetchItem({  ...defaultProps, ...props }, store, selector),
-    syncItem: (props)   => syncItem({   ...defaultProps, ...props }, store, selector),
-    syncList: (props)   => syncList({   ...defaultProps, ...props }, store, selector),
-    unsyncItem: (props) => unsyncItem({ ...defaultProps, ...props }),
-    unsyncList: (props) => unsyncList({ ...defaultProps, ...props })
-  };
+    fetchItem: props =>
+      fetchItem({ ...defaultProps, ...props }, store, selector),
+    syncItem: props => syncItem({ ...defaultProps, ...props }, store, selector),
+    syncList: props => syncList({ ...defaultProps, ...props }, store, selector),
+    unsyncItem: props => unsyncItem({ ...defaultProps, ...props }),
+    unsyncList: props => unsyncList({ ...defaultProps, ...props })
+  }
+}
 
-};
-
-export default getFirebaseSyncHelpers;
+export default getFirebaseSyncHelpers
